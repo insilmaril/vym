8bfef052ed85c8dbb0e65fa959d350dbe9bc5433
Uwe Drechsel <vym@insilmaril.de>
2011-03-11 12:44:39 +0100

    Version 1.13.28

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "1.13.27"
-#define __VYM_CODENAME "Codename: RC-3"
-//#define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2011-02-17"
+#define __VYM_VERSION "1.13.28"
+//#define __VYM_CODENAME "Codename: RC-3"
+#define __VYM_CODENAME "Codename: development version, not for production!"
+#define __VYM_BUILD_DATE "2011-03-11"
 
 
 bool checkVersion(const QString &);
