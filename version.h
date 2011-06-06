7e62a8a956d2b96b67a2b9e24f70c94003fe6de5
Uwe Drechsel <vym@insilmaril.de>
2011-06-06 14:11:43 +0200

    Version 1.13.32

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "1.13.31"
+#define __VYM_VERSION "1.13.32"
 //#define __VYM_CODENAME "Codename: RC-3"
 #define __VYM_CODENAME "Codename: development version, not for production!"
 #define __VYM_BUILD_DATE "2011-06-06"
