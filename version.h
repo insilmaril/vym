adc0a8289060a9df87801e71f93bce1a13d0e046
Uwe Drechsel <vym@insilmaril.de>
2011-10-14 12:31:37 +0200

    Revert "Version 2.0.0"
    This reverts commit ffcae36bf1eacc84f092ea2bcb200042e750ffb1.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.0.0"
-#define __VYM_CODENAME "Codename: Production release"
+#define __VYM_VERSION "1.99.1"
+#define __VYM_CODENAME "Codename: RC-1"
 //#define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2011-10-14"
+#define __VYM_BUILD_DATE "2011-10-10"
 
 
 bool checkVersion(const QString &);
