fc038ad09da0f53bd1bab441678ce0681be203ac
Uwe Drechsel <vym@insilmaril.de>
2011-11-29 23:46:49 +0100

    Removed some commented code

--- treedelegate.cpp
+++ treedelegate.cpp
@@ -10,8 +10,6 @@ TreeDelegate::TreeDelegate(QObject *)
 {
 }
 
-/*
-*/
 #include <QTextDocument>
 #include "noteobj.h"
 QString TreeDelegate::displayText (const QVariant & value, const QLocale & ) const  // FIXME-3 Ugly, but works...
@@ -25,33 +23,3 @@ QString TreeDelegate::displayText (const QVariant & value, const QLocale & ) con
     return s.trimmed().replace ('\n',' ').replace ('\t',' ');
 }
 
-/*
-void TreeDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
-{
-    QStyledItemDelegate::paint(painter, option, index);	//FIXME use own pant method to convert richtext 2 plaintext
-    return;
-    painter->save();
-
-    QRectF rect=option.rect;
-    QString text; //= qVariantValue <QString> (index.data());
-
-    TreeItem *ti=((TreeModel*)index.model())->getItem(index);
-
-    text=ti->getHeadingPlain();
-
-    // Paint highlighted, if selected	    //FIXME not as nice as original implementation with bright borders and gradient
-     if (option.state & QStyle::State_Selected)
-	painter->fillRect(option.rect, option.palette.highlight() );
-
-
-    painter->drawText(rect, Qt::AlignLeft, text );  
-
-    painter->restore();
-
-}
-QSize TreeDelegate::sizeHint(const QStyleOptionViewItem &option,
-                    const QModelIndex &index ) const
-{
-    return QStyledItemDelegate::sizeHint(option, index);    //FIXME sizeHint incorrect for RichText
-}
-*/
