0f2b6c6d9f823f8b92f9224d1219eae66af324f0
Uwe Drechsel <vym@insilmaril.de>
2012-06-13 11:20:28 +0200

    Version 2.2.0

--- version.h
+++ version.h
@@ -4,9 +4,9 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.1.13"
-//#define __VYM_CODENAME "Codename: Production release"
-#define __VYM_CODENAME "Codename: development version, not for production!"
+#define __VYM_VERSION "2.2.0"
+#define __VYM_CODENAME "Codename: Production release"
+//#define __VYM_CODENAME "Codename: development version, not for production!"
 #define __VYM_BUILD_DATE "2012-06-14"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
