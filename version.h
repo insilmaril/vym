782c172d62ced2222e2f2c7a6f96898b1d8f000e
Uwe Drechsel <vym@insilmaril.de>
2013-01-03 13:25:28 +0100

    Updating date

--- version.h
+++ version.h
@@ -8,7 +8,7 @@
 //#define __VYM_CODENAME "Codename: Production release"
 //#define __VYM_CODENAME "Codename: development version, not for production!"
 #define __VYM_CODENAME "Codename: christmas version, not for production!"
-#define __VYM_BUILD_DATE "2012-12-29"
+#define __VYM_BUILD_DATE "2013-01-03"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool checkVersion(const QString &);
