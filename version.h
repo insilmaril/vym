a1fc0d43469f1db9be0fe9152ac7fbae69b99997
Uwe Drechsel <vym@insilmaril.de>
2013-06-04 10:08:56 +0200

    Version 2.3.18

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.3.17"
+#define __VYM_VERSION "2.3.18"
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
 #define __VYM_BUILD_DATE "2013-06-03"
