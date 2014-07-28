72035a8be6642352910cffd4eca689e24b793696
Uwe Drechsel <vym@insilmaril.de>
2014-07-28 14:15:53 +0200

    Version 2.3.24

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.3.23"
+#define __VYM_VERSION "2.3.24"
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2014-03-26"
+#define __VYM_BUILD_DATE "2014-07-28"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool checkVersion(const QString &);
