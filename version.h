18cc08451aab698104dd7196202dc08360785d36
Uwe Drechsel <vym@insilmaril.de>
2015-03-06 09:49:48 +0100

    Bugfix: - Dataloss caused by  failing zip process
    Fixing regression of previous commit.

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #define __VYM_VERSION "2.4.0"
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2015-03-05"
+#define __VYM_BUILD_DATE "2015-03-06"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool versionLowerThanVym(const QString &);
