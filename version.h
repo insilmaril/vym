17010388119f11f978431d4462b449f216e2009d
Uwe Drechsel <vym@insilmaril.de>
2015-11-11 10:41:37 +0100

    Version 2.5.1

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.5.0"
-#define __VYM_CODENAME "Codename: Production release"
-//#define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2015-10-28"
+#define __VYM_VERSION "2.5.1"
+//#define __VYM_CODENAME "Codename: Production release"
+#define __VYM_CODENAME "Codename: development version, not for production!"
+#define __VYM_BUILD_DATE "2015-11-11"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool versionLowerThanVym(const QString &);
