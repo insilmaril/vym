64ddb77730a7d782721e16b1fd396f9bd8244525
Uwe Drechsel <vym@insilmaril.de>
2015-11-12 14:24:27 +0100

    New build date

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #define __VYM_VERSION "2.5.1"
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2015-11-11"
+#define __VYM_BUILD_DATE "2015-11-12"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool versionLowerThanVym(const QString &);
