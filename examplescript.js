3c6ac9570f61c3296aad0128f250ae8fd1621181
Uwe Drechsel <vym@insilmaril.de>
2016-09-29 14:38:59 +0200

    Updated

--- examplescript.js
+++ examplescript.js
@@ -1,25 +1,7 @@
-print("Current Heading: " + model.getHeadingPlainText() );
-/*
-if ( model.getHeadingPlainText() == "foobar" )
-{
-  print("  ...is 'foobar'");
-}   else    
-{
-  print("  ...is NOT 'foobar'");
-}
+//Example function definition
+function foofunction(s) {print ("Foofunction: " +  s);}
 
-//vym.toggleTreeEditor()
-*/
-print ("Current map: " + model.getFileName() );
-
-m1 = vym.getCurrentMap();
-print ("m1: " + m1.getFileName() );
-vym.selectMap(1);
-m2 = vym.getCurrentMap();
-print ("m1: " + m1.getFileName() );
-print ("m2: " + m2.getFileName() );
-vym.selectMap(0);
-m3 = vym.getCurrentMap();
-print ("m1: " + m1.getFileName() );
-print ("m2: " + m2.getFileName() );
-print ("m3: " + m3.getFileName() );
+foofunction("asdfs");
+abort("xxx");
+print ("bar");
+"ok, I'm done";
