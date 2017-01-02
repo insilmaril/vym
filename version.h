0f3f6e75ee5d92cd4b27d69cc5ea418274c7e3fd
Uwe Drechsel <vym@insilmaril.de>
2017-01-02 17:50:44 +0100

    Version 2.6.4

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #include <QString>
 
 #define __VYM_NAME "VYM" 
-#define __VYM_VERSION "2.6.3"
+#define __VYM_VERSION "2.6.4"
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
 #define __VYM_BUILD_DATE "2017-01-02"
