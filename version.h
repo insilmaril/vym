9989daf799b410e0eb86b7c74db9315c6ec01c7c
Uwe Drechsel <vym@insilmaril.de>
2017-05-19 08:39:53 +0200

    Version 2.6.9
    Bugfix: ImportDir

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM" 
-#define __VYM_VERSION "2.6.8"
+#define __VYM_VERSION "2.6.9"
 #define __VYM_CODENAME "Codename: Production release"
 //#define __VYM_CODENAME "Codename: development version, not for production!"
-#define __VYM_BUILD_DATE "2017-03-08"
+#define __VYM_BUILD_DATE "2017-05-19"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool versionLowerThanVym(const QString &);
