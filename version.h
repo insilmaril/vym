d792106c523de24ed75a3ccb958c6bffdc09311d
Uwe Drechsel <vym@insilmaril.de>
2018-06-10 10:37:09 +0200

    Version 2.6.209

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 #include <QString>
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.6.208" 
+#define __VYM_VERSION "2.6.209" 
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development leap version, not for production!"
-#define __VYM_BUILD_DATE "2018-06-07"
+#define __VYM_BUILD_DATE "2018-06-10"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 bool versionLowerThanVym(const QString &);
