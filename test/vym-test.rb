6f541a26728920903a02828dd93de464eaae5c39
Uwe Drechsel <vym@insilmaril.de>
2018-10-06 14:20:18 +0200

    Fixed version in test script

--- test/vym-test.rb
+++ test/vym-test.rb
@@ -100,7 +100,7 @@ end
 #######################
 def test_vym (vym)
   heading "Mainwindow checks:"
-  version = "2.6.207"
+  version = "2.6.214"
   expect "Version is #{version}", vym.version, version
 
   expect "Loading map '#{@testmap}'", vym.loadMap(@testmap), true
