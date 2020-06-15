8f3c54d40b4abe18aa3c9102084e8afb1801c52c
Uwe Drechsel <vym@insilmaril.de>
2020-06-15 13:08:01 +0200

    Version 2.7.509

--- version.h
+++ version.h
@@ -2,12 +2,12 @@
 #define VERSION_H
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.7.508" 
+#define __VYM_VERSION "2.7.509" 
 //#define __VYM_CODENAME "Codename: Production release"
 #define __VYM_CODENAME "Codename: development version, not for production!"
 //#define __VYM_CODE_QUALITY "production"
 #define __VYM_CODE_QUALITY "development"
-#define __VYM_BUILD_DATE "2020-04-22"
+#define __VYM_BUILD_DATE "2020-06-15"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
