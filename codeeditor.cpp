280b318ef9eefe05eada4c3cc4a2b56cc125a254
Uwe Drechsel <vym@insilmaril.de>
2021-01-25 15:14:00 +0100

    Updated FIXMEs

--- codeeditor.cpp
+++ codeeditor.cpp
@@ -26,7 +26,8 @@ int CodeEditor::lineNumberAreaWidth()
         ++digits;
     }
 
-    // FIXME-2 avail. in Qt 5.11:  int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+    // FIXME-3 avail. in Qt 5.11:  int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+    // Check Qt version in openSUSE before changing...
     int space = 3 + ( 4 + fontMetrics().boundingRect(QString("9")).width() ) * digits;;
 
     return space;
