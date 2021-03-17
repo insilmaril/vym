c695e3d2e495ccd7b156a721809d7f9907cef62c
Uwe Drechsel <vym@insilmaril.de>
2021-03-17 10:41:32 +0100

    Version 2.7.569
    Bugfix: Waking up of sleeping tasks is visible again

--- src/version.h
+++ src/version.h
@@ -2,12 +2,12 @@
 #define VERSION_H
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.7.568"
+#define __VYM_VERSION "2.7.569"
 #define __VYM_BUILD_DATE "2021-03-17"
 #define __VYM_CODE_QUALITY "Beta"
-//#define __VYM_CODE_QUALITY "production"
+//#define __VYM_CODE_QUALITY "Production"
+#define __VYM_CODENAME "(Beta 3 of upcoming 2.8.0)"
 //#define __VYM_CODENAME "Codename: Production release"
-#define __VYM_CODENAME "(2.8.0-beta-2+)"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
