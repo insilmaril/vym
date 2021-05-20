80b4ffa9f90b17f5d7253d5970b2a66c964e3a23
Uwe Drechsel <vym@insilmaril.de>
2021-05-20 13:31:37 +0200

    Version: 2.8.500

--- src/version.h
+++ src/version.h
@@ -2,10 +2,11 @@
 #define VERSION_H
 
 #define __VYM_NAME "VYM"
-#define __VYM_VERSION "2.8.2"
-#define __VYM_BUILD_DATE "2021-04-22"
+#define __VYM_VERSION "2.8.500"
+#define __VYM_BUILD_DATE "2021-05-17"
+#define __VYM_CODE_QUALITY "(Experimental)"
 //#define __VYM_CODE_QUALITY "(Beta)"
-#define __VYM_CODE_QUALITY "Production"
+//#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODENAME "(Beta 5 of upcoming 2.8.0)"
 #define __VYM_CODENAME ""
 #define __VYM_HOME "http://www.insilmaril.de/vym"
