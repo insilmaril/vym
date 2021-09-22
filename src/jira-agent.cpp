75e4d133afd3184071b6b3c813b2f3c700292b64
Uwe Drechsel <vym@insilmaril.de>
2021-09-22 11:30:18 +0200

    Enabled building in Qt 5.12 again

--- src/jira-agent.cpp
+++ src/jira-agent.cpp
@@ -230,7 +230,7 @@ void JiraAgent::ticketReceived(QNetworkReply *reply)
 
         qWarning() << "JiraAgent::ticketRReveived reply error";
         qWarning() << "Error: " << reply->error();
-        vout << "reply: " << Qt::endl << r << Qt::endl;
+        vout << "reply: " << endl << r << endl;
         finishJob();
         return;
     }
