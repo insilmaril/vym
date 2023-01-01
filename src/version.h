3ec7a03a881eb6abdeaa6d7b199de0f2679a3fb5
Uwe Drechsel <vym@insilmaril.de>
2023-01-01 21:17:04 +0100

    Minor update in version string

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 //#define __VYM_CODE_QUALITY "(Development)"
 //#define __VYM_CODE_QUALITY "(Beta)"
 //#define __VYM_CODENAME "(Beta 1 of upcoming 2.9.0)"
-#define __VYM_CODENAME "Happy new year!"
+#define __VYM_CODENAME ""
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
