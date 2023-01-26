932b1f6d1be88d88393120c277875498d7e4b94a
Uwe Drechsel <vym@insilmaril.de>
2023-01-26 10:43:09 +0100

    Updated comment again

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 //#define __VYM_CODE_QUALITY "(Development)"
 //#define __VYM_CODE_QUALITY "(Beta)"
 //#define __VYM_CODENAME "(Beta 1 of upcoming 2.9.0)"
-#define __VYM_CODENAME "The 'primes are nice' release'"
+#define __VYM_CODENAME "The 'primes are nice' release :-)"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
