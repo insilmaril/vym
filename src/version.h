48727597e0d036b241a6eebf59da0811f2683089
Uwe Drechsel <vym@insilmaril.de>
2023-02-14 12:36:18 +0100

    Updated build date and code quality info

--- src/version.h
+++ src/version.h
@@ -3,12 +3,11 @@
 
 #define __VYM_NAME "VYM"
 #define __VYM_VERSION "2.9.0"
-#define __VYM_BUILD_DATE "2023-01-26"
-#define __VYM_CODE_QUALITY "Production"
+#define __VYM_BUILD_DATE "2023-02-14"
+//#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODE_QUALITY "(Development)"
-//#define __VYM_CODE_QUALITY "(Beta)"
-//#define __VYM_CODENAME "(Beta 1 of upcoming 2.9.0)"
-#define __VYM_CODENAME "The 'primes are nice' release :-)"
+#define __VYM_CODE_QUALITY "(Beta)"
+#define __VYM_CODENAME "Beta 2 of upcoming 2.9.0"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
