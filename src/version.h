5f8c6f1b16ceba3d1335f918fcbe1d72b8824c72
Uwe Drechsel <vym@insilmaril.de>
2023-02-24 13:22:33 +0100

    Version 2.9.0 Beta 4
    Minor fix to transmit localeName

--- src/version.h
+++ src/version.h
@@ -3,11 +3,11 @@
 
 #define __VYM_NAME "VYM"
 #define __VYM_VERSION "2.9.0"
-#define __VYM_BUILD_DATE "2023-02-23"
+#define __VYM_BUILD_DATE "2023-02-24"
 //#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODE_QUALITY "(Development)"
 #define __VYM_CODE_QUALITY "*Beta*"
-#define __VYM_CODENAME "Beta 3 of upcoming 2.9.0"
+#define __VYM_CODENAME "Beta 4 of upcoming 2.9.0"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
