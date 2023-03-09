46d6e849d94709dc4790f5bc841e1c470494ff3a
Uwe Drechsel <vym@insilmaril.de>
2023-03-09 09:43:06 +0100

    Version 2.9.0 Beta 5

--- src/version.h
+++ src/version.h
@@ -1,13 +1,15 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_NAME "VYM"
 #define __VYM_VERSION "2.9.0"
 #define __VYM_BUILD_DATE "2023-03-09"
+
 //#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODE_QUALITY "(Development)"
 #define __VYM_CODE_QUALITY "*Beta*"
 #define __VYM_CODENAME "Beta 5 of upcoming 2.9.0"
+
+#define __VYM_NAME "VYM"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
 #endif
