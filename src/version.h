fb8ea89ab3b82faea00a021e30509d34addaa513
Uwe Drechsel <vym@insilmaril.de>
2023-05-15 12:58:43 +0200

    Version 2.9.3

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "2.9.2"
-#define __VYM_BUILD_DATE "2023-04-13"
+#define __VYM_VERSION "2.9.3"
+#define __VYM_BUILD_DATE "2023-05-15"
 
 #define __VYM_CODE_QUALITY "Production"
 #define __VYM_CODENAME ""
