d5303196fade5049282b84d9072114013c9398f4
Uwe Drechsel <vym@insilmaril.de>
2023-07-10 14:54:48 +0200

    Updated FIXMEs

--- src/vymmodel.cpp
+++ src/vymmodel.cpp
@@ -2192,7 +2192,7 @@ void VymModel::setURL(QString url, bool updateFromCloud, BranchItem *bi)
         emitDataChanged(bi);
         reposition();
 
-        if (updateFromCloud)
+        if (updateFromCloud)    // FIXME-2 use oembed.com also for Youtube and other cloud providers
             // Check for Confluence
             setHeadingConfluencePageName();
     }
