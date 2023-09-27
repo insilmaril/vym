bad93ba332a615fe67fe2f68e8be6877ef1fd9da
Uwe Drechsel <vym@insilmaril.de>
2023-09-27 12:32:19 +0200

    Removed debug output

--- src/xlink.cpp
+++ src/xlink.cpp
@@ -65,7 +65,6 @@ BranchItem *Link::getEndBranch() { return endBranch; }
 void Link::setEndPoint(QPointF p)
 {
     // Used only while creating the link, without endBranch
-    qDebug() << "Link::setEntPoint p=" << toS(p);
     if (xlo)
         xlo->setEnd(p);
 }
