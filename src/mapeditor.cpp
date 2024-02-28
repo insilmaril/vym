9bd64f75b4b70d6fe3ed76711edc441356c6a4ba
Uwe Drechsel <vym@insilmaril.de>
2024-02-28 11:10:46 +0100

    Updated FIXMEs

--- src/mapeditor.cpp
+++ src/mapeditor.cpp
@@ -2447,7 +2447,7 @@ void MapEditor::mouseDoubleClickEvent(QMouseEvent *e)
     }
 }
 
-void MapEditor::wheelEvent(QWheelEvent *e)  // FIXME-2 for zooming use current pos as center, not middle of view
+void MapEditor::wheelEvent(QWheelEvent *e)  // FIXME-1 for zooming use current pos as center, not middle of view
 {
     if (e->modifiers() & Qt::ControlModifier &&
         e->angleDelta().y() != 0) {
