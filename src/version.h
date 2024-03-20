8b447a36b6141259053ca34bb4011da992dead9f
Uwe Drechsel <vym@insilmaril.de>
2024-03-20 16:48:31 +0100

    Version 2.9.524

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "2.9.523"
-#define __VYM_BUILD_DATE "2024-03-18"
+#define __VYM_VERSION "2.9.524"
+#define __VYM_BUILD_DATE "2024-03-20"
 
 #define __VYM_NAME "VYM"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
