ca11b5f5962d51b2c31b711973760d5a35a0ea25
Bleyddyn <bleyddyn.aprhys@gmail.com>
2024-08-11 10:40:43 -0400

    Changed menu item name to just 'Clear'. For Issue #95.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1444,7 +1444,7 @@ void Main::setupFileActions()
     fileLastMapsMenu = fileMenu->addMenu(tr("Open Recent", "File menu"));
     fileMenu->addSeparator();
 
-    a = new QAction(tr("&Clear Menu", "Open Recent menu"), this);
+    a = new QAction(tr("&Clear", "Open Recent menu"), this);
     a->setEnabled(false);
     connect(a, SIGNAL(triggered()), this, SLOT(fileClearRecent()));
     fileLastMapsMenu->addAction(a);
