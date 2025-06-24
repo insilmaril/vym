c1aba7196372e5bc501360b9a237581a1703330f
Uwe Drechsel <vym@insilmaril.de>
2025-06-24 14:30:45 +0200

    Version 2.9.577 - First beta release of 3.0.0

--- src/version.h
+++ src/version.h
@@ -1,16 +1,15 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "2.9.576"
-#define __VYM_BUILD_DATE "2025-06-02"
+#define __VYM_VERSION "2.9.577"
+#define __VYM_BUILD_DATE "2025-06-24"
 
 #define __VYM_NAME "VYMng"     // FIXME "next generation" in in window title
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 //
 //#define __VYM_CODE_QUALITY "Production"
-#define __VYM_CODE_QUALITY "*Experimental*"
-//#define __VYM_CODE_QUALITY "*Beta*"
-//#define __VYM_CODENAME "Alpha 1 of upcoming 3.0.0"
-#define __VYM_CODENAME "2025 21 year anniversary edition"
+//#define __VYM_CODE_QUALITY "*Experimental*"
+#define __VYM_CODE_QUALITY "*Beta*"
+#define __VYM_CODENAME "Beta release of upcoming 3.0.0"
 
 #endif
