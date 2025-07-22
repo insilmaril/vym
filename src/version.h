91eee52724ac1f37f209d19f12e9c8df56dda694
Uwe Drechsel <vym@insilmaril.de>
2025-07-22 12:36:36 +0200

    Version 2.9.585

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "2.9.584"
-#define __VYM_BUILD_DATE "2025-07-21"
+#define __VYM_VERSION "2.9.585"
+#define __VYM_BUILD_DATE "2025-07-22"
 
 #define __VYM_NAME "VYMng"     // FIXME "next generation" in in window title
 #define __VYM_HOME "http://www.insilmaril.de/vym"
