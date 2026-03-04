e9c4d38d2d89a0e7cd8035174a84917160cf5896
Uwe Drechsel <vym@insilmaril.de>
2026-03-04 12:20:59 +0100

    Cleanup version file

--- src/version.h
+++ src/version.h
@@ -5,13 +5,13 @@
 #define __VYM_BUILD_DATE "2026-02-20"
 
 
-#define __VYM_NAME "VYMng"     // FIXME "next generation" in in window title
+#define __VYM_NAME "VYM"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
-//
+
 //#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODE_QUALITY "*Experimental*"
+
 #define __VYM_CODE_QUALITY "*Beta*"
 #define __VYM_CODENAME "Beta release of upcoming 3.0.0"
-//#define __VYM_CODENAME "Debug version of upcoming 3.0.0"
 
 #endif
