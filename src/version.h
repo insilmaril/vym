fa84942b35e6f8851b4c918c7d9aadf43b6ef705
Uwe Drechsel <vym@insilmaril.de>
2026-07-04 12:49:25 +0200

    Version 2.9.617

--- src/version.h
+++ src/version.h
@@ -1,17 +1,17 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "2.9.616"
-#define __VYM_BUILD_DATE "2026-05-21"
+#define __VYM_VERSION "2.9.617"
+#define __VYM_BUILD_DATE "2026-06-04"
 
 
 #define __VYM_NAME "VYM"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
 
-//#define __VYM_CODE_QUALITY "Production"
+#define __VYM_CODE_QUALITY "Production"
 //#define __VYM_CODE_QUALITY "*Experimental*"
+//#define __VYM_CODE_QUALITY "*Beta*"
 
-#define __VYM_CODE_QUALITY "*Beta*"
 #define __VYM_CODENAME "Beta release of upcoming 3.0.0"
 
 #endif
