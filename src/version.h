2e0b572b7e7328c0a316e11ace4a77980e4d7685
Uwe Drechsel <vym@insilmaril.de>
2026-07-29 08:53:00 +0200

    Version 3.0.502

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define __VYM_VERSION "3.0.501"
+#define __VYM_VERSION "3.0.502"
 
 #define __VYM_NAME "VYM"
 #define __VYM_HOME "http://www.insilmaril.de/vym"
